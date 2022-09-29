01b703f245bf9cb0700fac725469c5174c3c9d8c
SACHIN SIKARWAR <sachin.plac@gmail.com>
2022-09-29 17:03:38 +0530

    new

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+const inputs = document.querySelectorAll('.comment');
+
+for (const input of inputs) {
+
+  input.setAttribute('required', '');
+
+}
+
+const textarea = document.getElementById('message');
+$(document).ready(function(){
+  $("textarea").click(function(){
+    textarea.value='';
+  });
+});
