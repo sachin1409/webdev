417deb843cfba16ddb2c0e0f35ff9c7f6d8d76ed
sachin <sachin.plac@gmail.com>
2022-10-18 10:51:30 +0530

    commit

--- main.js
+++ main.js
@@ -7,16 +7,19 @@
 
  
 
-const btn = document.getElementById('d');
-btn.addEventListener('click', function handleClick(event) {
-
-  event.preventDefault();
-const clear = document.getElementById('uname');
- console.log([clear.value]);
+const btn = document.getElementById('d'); //button id
+btn.addEventListener('click', function re() {
+const clear = document.getElementById('uname'); //comment box id
+  console.log([clear.value]); // it will clear the comment box
   clear.value = '';
 });
 
-
+//ALERT-POP UP
 function clicked(){
+  if(uname.value.length==0) {
+    alert('PLEASE ADD A COMMENT');
+  }
+  else{
   alert('Comment Added Successfully as -'+uname.value);
+  }
 }
