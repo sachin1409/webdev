1a3bd51ebe7dd6d8836f95eded0d68d2d11979a4
sachin <sachin.plac@gmail.com>
2022-10-18 11:45:11 +0530

    commit

--- main.js
+++ main.js
@@ -1,25 +1,12 @@
-
-      // function send() {
-      //   let uname = document.querySelector("#uname");
-      //   console.log([uname.value]);
-        
-      //  }
-
- 
-
 const btn = document.getElementById('d'); //button id
 btn.addEventListener('click', function re() {
 const clear = document.getElementById('uname'); //comment box id
   console.log([clear.value]); // it will clear the comment box
   clear.value = '';
-});
-
-//ALERT-POP UP
-function clicked(){
   if(uname.value.length==0) {
     alert('PLEASE ADD A COMMENT');
   }
   else{
   alert('Comment Added Successfully as -'+uname.value);
   }
-}
+});
