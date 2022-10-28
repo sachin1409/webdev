771d367c1aeaba0289619b234f77e144c96c5cc3
sachin <sachin.plac@gmail.com>
2022-10-28 17:12:37 +0530

    commit

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
-const btn = document.getElementById('d'); //button id
-btn.addEventListener('click', function re() {
-const clear = document.getElementById('uname'); //comment box id
-  console.log([clear.value]); // it will clear the comment box
-  clear.value = '';
+function clicked(){
+  var name1=window.prompt("Enter your name");  
   if(uname.value.length==0) {
     alert('PLEASE ADD A COMMENT');
   }
   else{
   alert('Comment Added Successfully as -'+uname.value);
   }
-});
+  
+  const comm = document.getElementById('uname');
+  let dat=new Date();  
+  
+  //taking comment of user 
+  let user ={
+    name:name1,
+    comment:comm.value,
+    userID:Math.floor(Math.random() * 100),
+    Date:dat
+  }
+  document.getElementById("etext").innerHTML = "New text!";
+    const myArray = Object.values(user);
+    console.log(`Name Of the User:  ${myArray[0]}   Comment: ${myArray[1]}  UserId : ${myArray[2]} Date : ${myArray[3]}`);
+    alert(`Name Of the User:  ${myArray[0]}   Comment: ${myArray[1]}  UserId : ${myArray[2]} Date : ${myArray[3]}`);
+    comm.value = '';
+
+}
+
+
+// const cir= new clicked();
+
+// for (let key in cir){
+//   console.log(key,cir[key]);
+// }
