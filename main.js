eee094f5aea5ffa39e184124b9e81eefd98140fb
sachin <sachin.plac@gmail.com>
2022-10-31 11:14:44 +0530

    commit

--- main.js
+++ main.js
@@ -1,33 +1,40 @@
-function clicked(){
-  var name1=window.prompt("Enter your name");  
-  if(uname.value.length==0) {
+function clicked() {
+  var name1 = window.prompt("Enter your name");
+  if (uname.value.length == 0) {
     alert('PLEASE ADD A COMMENT');
   }
-  else{
-  alert('Comment Added Successfully as -'+uname.value);
+  else {
+    alert('Comment Added Successfully as -' + uname.value);
   }
-  
-  const comm = document.getElementById('uname');
-  let dat=new Date();  
-  
-  //taking comment of user 
-  let user ={
-    name:name1,
-    comment:comm.value,
-    userID:Math.floor(Math.random() * 100),
-    Date:dat
-  }
-  document.getElementById("etext").innerHTML = "New text!";
-    const myArray = Object.values(user);
-    console.log(`Name Of the User:  ${myArray[0]}   Comment: ${myArray[1]}  UserId : ${myArray[2]} Date : ${myArray[3]}`);
-    alert(`Name Of the User:  ${myArray[0]}   Comment: ${myArray[1]}  UserId : ${myArray[2]} Date : ${myArray[3]}`);
-    comm.value = '';
 
+  //Comment List
+
+  let commentBoxValue = document.getElementById('uname').value;
+  let h2 = document.createElement("h2");
+  let text = document.createTextNode(commentBoxValue);
+  
+ document.getElementById("unordered").innerHTML=(`Comment is :${commentBoxValue}`);
+
+
+
+
+  const comm = document.getElementById('uname');
+  let dat = new Date();
+  //taking comment of user 
+  let user = {
+    name: name1,
+    comment: comm.value,
+    userID: Math.floor(Math.random() * 100),
+    Date: dat
+  }
+
+  const myArray = Object.values(user);
+  console.log(`Name Of the User:  ${myArray[0]}   Comment: ${myArray[1]}  UserId : ${myArray[2]} Date : ${myArray[3]}`);
+  alert(`Name Of the User:  ${myArray[0]}   Comment: ${myArray[1]}  UserId : ${myArray[2]} Date : ${myArray[3]}`);
+  comm.value = '';
 }
 
 
-// const cir= new clicked();
 
-// for (let key in cir){
-//   console.log(key,cir[key]);
-// }
+
+
